0973eb6f5038d6cd212099af006571c3df932c1d
Iwubox <arvin@framecore.se>
2025-02-06 22:23:48 +0100

    sliderFinished

--- Script.js
+++ Script.js
@@ -2,45 +2,58 @@ let darkmode = false;
 let language = "Swedish";
 let simple = false;
 
-//darkmode
-//onclick
-if (darkmode === false) {
-  darkmode = true;
-} else if (darkmode === true) {
-  darkmode = false;
-} else {
-  console.log("error darkmode");
-}
+darkmode = () => {
+  if (darkmode === false) {
+    darkmode = true;
+  } else if (darkmode === true) {
+    darkmode = false;
+  } else {
+    console.log("error darkmode");
+  }
+};
 
-//language
-//onclick
-if (language === "Swedish") {
-  language = "English";
-} else if (language === "English") {
-  language = "Swedish";
-} else {
-  console.log("error language");
-}
-
-let simpleSite = document.getElementsByClassName("simpleSite")[0];
-let advancedSite = document.getElementsByClassName("advancedSite")[0];
+language = () => {
+  if (language === "Swedish") {
+    language = "English";
+  } else if (language === "English") {
+    language = "Swedish";
+  } else {
+    console.log("error language");
+  }
+};
 
 let slider = document.getElementsByClassName("sliderCircleHB");
 
 slider[0].classList.add("sliderOff");
-
 toggleSimple = () => {
   if (simple === false) {
     simple = true;
+    changeSiteSimplicity(simple);
 
     slider[0].classList.add("sliderOn");
     slider[0].classList.remove("sliderOff");
   } else if (simple === true) {
     simple = false;
+    changeSiteSimplicity(simple);
 
     slider[0].classList.remove("sliderOn");
     slider[0].classList.add("sliderOff");
   } else {
     console.log("error simple");
+  }
+};
+
+let simplestSite = document.getElementsByClassName("simplestSite");
+let advancedSite = document.getElementsByClassName("advancedSite");
+
+changeSiteSimplicity = (Simple) => {
+  if (Simple === true) {
+    simplestSite[0].style.display = "block";
+    advancedSite[0].style.display = "none";
+  } else if (Simple === false) {
+    simplestSite[0].style.display = "none";
+    advancedSite[0].style.display = "block";
+  } else {
+    console.log("error simpleSite");
   }
 };
